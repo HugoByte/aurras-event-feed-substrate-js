c287f84ab8399a33af92a6786d0a3427f19e63f7
Muhammed Irfan K <muhammedirfan@hugobyte.com>
2021-01-02 10:37:40 +0530

    Added logic to throw configuration exception

--- src/modules/configuration.module.ts
+++ src/modules/configuration.module.ts
@@ -2,19 +2,22 @@ import { MicroframeworkSettings, MicroframeworkLoader } from 'microbootstrap';
 import { util } from 'config';
 import { buildYup } from 'schema-to-yup';
 import * as jsonSchema from '../../config/schema.json';
-import { InvalidConfigurationException } from "@exceptions";
+import { ConfigurationException } from "@exceptions";
+
 /**
  * Configuration Module validates the config passed through environment variables with the schema provided in /config/schema.json.
  * Config is parsed and loaded using https://github.com/lorenwest/node-config.
  * Config validation is done using https://github.com/jquense/yup.
  */
-export const ConfigurationModule: MicroframeworkLoader = (settings: MicroframeworkSettings | undefined) => {
-    if (settings) {
+
+export const ConfigurationModule: MicroframeworkLoader = (frameworkSettings: MicroframeworkSettings | undefined) => {
+    
+    if (frameworkSettings) {
         // Build Yup schema from the provided json schema.
         const yupSchema = buildYup(jsonSchema, {});
+
         // Using synchronous validation as we need to be sure that configuration are valid before loading other modules.
-        try { yupSchema.validateSync(util.loadFileConfigs()) } catch (err) {
-            
-        }
+        try { yupSchema.validateSync(util.loadFileConfigs()) }
+        catch (error) { throw new ConfigurationException(error.message); }
     }
 }
