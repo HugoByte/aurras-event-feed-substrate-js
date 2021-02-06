19717b5c858789bf402acbb7b1451acce70b5278
Muhammed Irfan <50794925+MuhammedIrfan@users.noreply.github.com>
2021-02-06 10:38:34 +0530

    Remove chain and openwhisk integration testing (#29)

--- tests/chain.test.ts
+++ tests/chain.test.ts
@@ -1,8 +1,12 @@
 import { chainProvider } from '../src/modules/chain.module';
 import { Observable } from 'rxjs'
+import { ChainService } from '../src/services/chain.service';
 
 describe('Chain Module Unit Tests', () => {
     test('Can get instance of observable', () => {
-        expect(chainProvider({ endpoint: "ws://localhost:9944", types: undefined, options: { reconnect: false }})).toBeInstanceOf(Observable);
+        let chainService = new ChainService();
+        chainService.api = chainProvider({ endpoint: "ws://localhost:9944", types: undefined, options: { reconnect: false }});
+
+        expect(chainService.api).toBeInstanceOf(Observable);
     });
 });
--- tests/event.test.ts
+++ tests/event.test.ts
@@ -1,60 +1,6 @@
-import { Observable, from } from 'rxjs';
-import { WsProvider, ApiRx, ApiPromise } from '@polkadot/api';
-import { EventService, ChainService } from '../src/services';
-import openwhisk from 'openwhisk';
-import { switchMap, delay } from 'rxjs/operators';
-
-function createRxApi(): Observable<ApiRx> {
-    jest.setTimeout(30000);
-
-    const provider = new WsProvider('ws://localhost:9944');
-
-    return new ApiRx({ provider }).isReady;
-}
-
-function createApi(): Promise<ApiPromise> {
-    jest.setTimeout(30000);
-
-    const provider = new WsProvider('ws://localhost:9944');
-
-    return new ApiPromise({ provider }).isReady;
-}
+import { EventService } from '../src/services';
 
 describe('Events Module Unit Tests', () => {
-    test('Can get instance of Observable from chain service', async (done) => {
-        const chainService = new ChainService();
-        chainService.api = createRxApi();
-
-        expect(chainService.api).toBeInstanceOf(Observable);
-
-        (await chainService.api.toPromise()).disconnect();
-        done();
-    });
-
-    test('Can listen to events from the chain', (done) => {
-        const chainService = new ChainService();
-        chainService.api = createRxApi();
-
-        chainService.listenForEvents().subscribe(async (events) => {
-            expect(events).toBeTruthy();
-
-            (await chainService.api.toPromise()).disconnect();
-
-            done();
-        })
-    });
-
-    test('Can extract events from the chain', async (done) => {
-        const api = await createApi();
-        const events = api.query.system.events();
-        const eventService = new EventService();
-
-        expect(eventService.getEvents(events)).toBeTruthy();
-
-        api.disconnect();
-        done();
-    });
-
     test('Can filter events from all methods of the section', async () => {
         const eventService = new EventService();
         const mockEvents = [
@@ -92,36 +38,29 @@ describe('Events Module Unit Tests', () => {
         ]);
     });
 
-    test('Can post events to trigger manager', async (done) => {
+    test('Can extract event from the block records', () => {
         const eventService = new EventService();
-        const openwhiskApi = openwhisk({ apihost: "https://localhost:31001", api_key: "23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP", namespace: "guest", ignore_certs: true });
-        const event = {
-            section: "system",
-            method: "ExtrinsicSuccess",
-            data: [{
-                DispatchInfo: JSON.stringify({
-                    weight: 159133000,
-                    class: "Mandatory"
-                })
-            }]
-        }
+        const mockRecord = [
+            {
+                "event": {
+                    "section": "system",
+                    "method": "ExtrinsicSuccess",
+                    "meta": {
+                        "documentation": "Test event"
+                    },
+                    "typeDef": [{ displayName: undefined, info: 6, name: undefined, type: 'DispatchInfo' }],
+                    "data": [{ toString: () => '{ "weight": 159133000, "class": "Mandatory", "paysFee": "Yes" }' }]
+                }
+            }
+        ]
 
-        eventService.openwhiskApi = openwhiskApi;
-        eventService.invokeAction({ brokers: ["localhost:9092"], topic: "subsrate", event, action: "event-receiver" })
-            .subscribe((response) => {
-                expect(response).toStrictEqual({
-                    brokers: ['localhost:9092'],
-                    message: 'Event received from system',
-                    topic: 'subsrate',
-                    event: {
-                        section: "system", 
-                        method: "ExtrinsicSuccess", 
-                        data: [{
-                            DispatchInfo: "{\"weight\":159133000,\"class\":\"Mandatory\"}"
-                        }]
-                    }
-                })
-                done();
-            })
-    });
+        expect(eventService.getEvents(mockRecord)).toStrictEqual([{
+            "data": [{
+                "DispatchInfo": '{ "weight": 159133000, "class": "Mandatory", "paysFee": "Yes" }'
+            }],
+            "meta": "Test event",
+            "method": "ExtrinsicSuccess",
+            "section": "system"
+        }]);
+    })
 });
