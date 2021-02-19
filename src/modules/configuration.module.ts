f28e377dc124e4cbb9492e983707e3545d539865
Muhammed Irfan <50794925+MuhammedIrfan@users.noreply.github.com>
2021-02-19 17:22:56 +0530

    [BugFix] Update schema validation (#37)
    * Update schema validation library
    
    * Update JSON schema
    
    * Fix code styling
    
    * Update Configuration module and test

--- src/modules/configuration.module.ts
+++ src/modules/configuration.module.ts
@@ -1,14 +1,14 @@
 import { MicrobootstrapSettings, MicrobootstrapLoader } from '@hugobyte/microbootstrap';
 import { util } from 'config';
-import { buildYup } from 'schema-to-yup';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import { ConfigurationException } from "@exceptions/index";
+import Ajv, { DefinedError } from "ajv"
 
 /**
  * Configuration Module validates the config passed through environment variables with the schema provided in /config/schema.json.
  * Config is parsed and loaded using https://github.com/lorenwest/node-config.
- * Config validation is done using https://github.com/jquense/yup.
+ * Config validation is done using https://github.com/ajv-validator/ajv.
  */
 
 export const ConfigurationModule: MicrobootstrapLoader = (frameworkSettings: MicrobootstrapSettings | undefined) => {
@@ -20,13 +20,21 @@ export const ConfigurationModule: MicrobootstrapLoader = (frameworkSettings: Mic
             validateConfiguration({ schema, configuration });
         }
         catch (error) {
-            if (typeof error === 'string') throw new ConfigurationException(error);
-            throw new ConfigurationException(error.message);
+            if (typeof error === 'string') throw new ConfigurationException("", error);
+            throw new ConfigurationException(error.dataPath, error.message);
         }
     }
 }
 
 export const validateConfiguration = ({ schema, configuration }) => {
-    const yupSchema = buildYup(schema, {});
-    return yupSchema.validateSync(configuration)
+    const ajv = new Ajv();
+    const validate = ajv.compile(schema);
+    const isValidConfiguration = validate(configuration);
+
+    if (!isValidConfiguration) {
+        for (const error of validate.errors as DefinedError[]) {
+            throw error;
+        }
+    }
+    return isValidConfiguration;
 }
